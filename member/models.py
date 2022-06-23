235f4fb86a6ce925af5c0275e0e3b60bac8c9351
jaeeun <nakamotoryo99@gmail.com>
2022-06-23 12:04:55 +0900

    member join views, template 수정 - Member 객체 생성후 DB저장, 클라이언트/서버 유효성검사 추가, 가입완료페이지 전환

--- member/models.py
+++ member/models.py
@@ -1,3 +1,21 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+# member 테이블 구조와 유사하게 member 모델 정의
+class Member(models.Model):
+    id=models.AutoField(primary_key=True)
+    userid=models.CharField(max_length=18, unique=True)
+    passwd=models.CharField(max_length=18) #유니크 아님
+    name=models.CharField(max_length=5)
+    email=models.CharField(max_length=100)
+    regdate=models.DateTimeField(default=datetime.now)
+
+    #DB테이블 이름 지정
+    class Meta:
+        db_table = 'member'
+        ordering = ['-regdate'] #- DESC +ASC
+
+    def __str__(self):
+        return self.userid  #객체출력시 출력형태 정의
--- member/views.py
+++ member/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
+from member.models import Member
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
+#회원가입 처리함수
 def join(request):
-    return render(request, 'member/join.html')
+    returnPage = 'member/join.html'
+
+    if request.method == 'GET':
+        return render(request, returnPage)
+
+    elif request.method == 'POST':
+        form = request.POST.dict() #폼 데이터 다 들고 와서 dict형태로 저장
+        # print(form, form['userid'])
+
+        #유효성 검사
+
+        error = ''
+        if not (form['userid'] and form['pwd'] and form['pwd2'] and form['name'] and form['email']):
+            error = '<li>입력값을 다시 확인해라</li>'
+        elif form['pwd'] != form['pwd2']:
+            error = '<li>비밀번호가 일치하지 않긔</li>'
+        else:
+            member = Member( #클래스 생성자. 빨간줄 alr Enter로
+                userid=form['userid'], pwd=make_password(form['pwd']), name=form['name'], email=form['email'],
+            )
+
+            member.save()
+
+            returnPage = 'member/joinok.html'
+
+
+        context = {'form':form, 'error': error} #오류를 보내기 위해 dict 변수에 저장
+
+        return render(request, returnPage, context)
+
 
 
 def login(request):
