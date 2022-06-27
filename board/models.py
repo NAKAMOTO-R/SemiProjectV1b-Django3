22589b6dbc95f81bf5322584a3dbb5768e02c78d
jaeeun <nakamotoryo99@gmail.com>
2022-06-27 17:34:48 +0900

    django3 board 리스트/본문글출력, 본문글 수정/삭제, 조회수추가, 리스트 no cache, 본문글 줄바꿈, 리스트 글번호 정리

--- board/models.py
+++ board/models.py
@@ -4,10 +4,10 @@ from member.models import Member
 # Create your models here.
 
 #on_delete : CASCADE, DO_NOTHING
-class board(models.Model):
+class Board(models.Model):
     id=models.AutoField(primary_key=True)
     title=models.CharField(max_length=100)
-    userid=models.ForeignKey(Member, db_column='userid', on_delete=models.DO_NOTHING) # on_delete=models.CASCADE 회원정보 지우면 게시물 삭제
+    member=models.ForeignKey(Member, related_name='board', on_delete=models.DO_NOTHING) # on_delete=models.CASCADE 회원정보 지우면 게시물 삭제
     regdate=models.DateTimeField(default=datetime.now)
     views=models.IntegerField(default=0)
     contents=models.TextField(null=False, blank=False) #false는 not null이라는 뜻
--- board/views.py
+++ board/views.py
@@ -1,18 +1,45 @@
+from django.db.models import F
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from board.models import board
+from django.views.decorators.cache import cache_control
+
 from member.models import Member
+from board.models import Board
 
-
+@cache_control(no_cache=True, no_store=True, must_revalidate=True)
 def list(request):
-    bdlist = board.objects.values('id','title','userid','regdate','views').order_by('-id')
+    # Board와 Member테이블이 userid id 컬럼 기준으로 inner join 수행
+    # bdlist.get(0).member.userid
+    # bdlist = board.objects.values('id','title','userid','regdate','views').order_by('-id')
+    # 쿼리셋 select 'id','title','userid','regdate','views' from borad order by id desc
+    bdlist = Board.objects.select_related('member')
 
     context = {'bds':bdlist}
     return render(request, 'board/list.html', context)
 
 def view(request):
-    return render(request, 'board/view.html')
+    if request.method == 'GET':
+        form = request.GET.dict()
+
+        # 조회수 처리 장고 ORM사용
+        # update board set views = views + 1 where id = 글번호..
+
+        # b = Board.objects.get(id=form['bno'])
+        # b.views = b.views + 1
+        # b.save()
+        Board.objects.filter(id=form['bno']).update(views=F('views') + 1)
+
+        bd = Board.objects.select_related('member').get(id=form['bno'])
+
+        # select * from board inner join member using(id) where id = bno..
+    # print(form['bno'])
+    elif request.method == 'POST':
+        pass
+
+    context = {'bd': bd}
+
+    return render(request, 'board/view.html', context)
 
 
 def write(request):
@@ -30,7 +57,7 @@ def write(request):
         if not (form['title'] and form['contents']):
             error = '제목이나 본문 작성혀라'
         else:
-            bd = board(title=form['title'], contents=form['contents'], userid=Member.objects.get(pk=form['userid']))
+            bd = Board(title=form['title'], contents=form['contents'], member=Member.objects.get(pk=form['memberid']))
 
             bd.save()
 
@@ -39,3 +66,37 @@ def write(request):
     context = {'form': form, 'error': error}
 
     return render(request, returnPage, context)
+
+#/remove?bno= 를 받아서 삭제하면 됨
+def remove(request):
+    if request.method == 'GET':
+        form = request.GET.dict()
+        Board.objects.filter(id=form['bno']).delete()
+        # delete from board where bno = 글번호..
+
+    return redirect('/list')
+
+
+def modify(request):
+    if request.method == 'GET':
+        form = request.GET.dict()
+        bd = Board.objects.get(id=form['bno'])
+        # select * from board where bno = 글번호..
+
+
+    elif request.method == 'POST':
+        form = request.POST.dict()
+        Board.objects.filter(id=form['bno']).update(title=form['title'], contents=form['contents'])
+
+        # update board set title = ???, contents = ?? where bno = 글번호
+        # b = Board.objects.get(id=form['bno'])
+        # b.title = form['title']
+        # b.contents = form['contents']
+        # b.save()
+
+
+        return redirect('/view?bno=' + form['bno'])
+
+
+    context = {'bd': bd}
+    return render(request, 'board/modify.html', context)
